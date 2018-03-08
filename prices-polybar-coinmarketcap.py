7399b0a640309d16aa25978fec0ab8fa54b4bf82
triplenull <tripleNull@protonmail.com>
2018-03-08 18:55:30 -0500

    added try/except block to avoid a ValueError when casting to float, print - instead

--- prices-polybar-coinmarketcap.py
+++ prices-polybar-coinmarketcap.py
@@ -1,6 +1,9 @@
 import sys
 import netcris
 
-pricef = float(netcris.retrieve_price(sys.argv[1]))
-print('{:.{prec}f}'.format(pricef, prec=0))
-
+# surreounded this in a try/except block as retrieve_price can return a string of '-' when there is no internet connection
+try:
+    pricef = float(netcris.retrieve_price(sys.argv[1]))
+    print('{:.{prec}f}'.format(pricef, prec=0))
+except ValueError:
+    print('-')
